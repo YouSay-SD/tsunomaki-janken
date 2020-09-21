025438ae51d2a27b0f3487a287a07a244807703f
YouSay-SD <yousay.sd@gmail.com>
2020-09-21 01:10:07 -0300

    Create HomeScreen Component

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import bg from '../../watame-background.png';
+import { Sidebar } from './Sidebar';
+
+const HomeStyled = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+`;
 
 export const HomeScreen = () => {
   return (
-    <div>
-        asdasd
-    </div>
+    <HomeStyled
+      style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url(${ bg })`,
+        backgroundPosition: 'center'
+      }}
+    >
+
+      <Sidebar />
+
+    </HomeStyled>
   )
 }
