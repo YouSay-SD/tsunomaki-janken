595328cedd81dd2b9caecbd9f226658a2cf455ef
YouSay-SD <yousay.sd@gmail.com>
2020-10-10 21:48:30 -0300

    create: home page

--- src/components/homeScreen/styles/homeScreen.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/macro';
-
-export const Content = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-image: url('/images/backgrounds/watame-background.png');
-  background-size: cover;
-  background-position: center;
-`;
