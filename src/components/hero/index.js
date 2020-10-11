27a448a8358d0cb7b3dd1457f3eb9fa78a14f15c
YouSay-SD <yousay.sd@gmail.com>
2020-10-10 23:57:35 -0300

    create: HeroContainer

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import { Container, Title } from './styles/hero';
-// import { Sidebar } from '../sidebar/Sidebar';
 
-export default function Hero() {
-  return (
-      <Hero.Container>
-        
-        {/* <Sidebar /> */}
-
-        <Hero.Title>
-          asdasdas
-        </Hero.Title>
-        
-      </Hero.Container>
-  )
-}
-
-Hero.Container = function HeroContainer({ children, ...restProps }) {
+export default function Hero({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Hero from '../components/hero';
+import { HeroContainer } from '../containers/hero';
 
 export default function Home() {
   return (
     <>
 
-      <Hero />
+      <HeroContainer />
 
     </>
   )
