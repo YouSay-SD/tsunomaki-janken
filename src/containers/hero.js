f71181272d0d34878b66ea5f63a450b6670e5dc5
YouSay-SD <yousay.sd@gmail.com>
2020-10-18 19:35:18 -0300

    added: start transitions backgrounds when document has focus

--- src/containers/hero.js
+++ src/containers/hero.js
@@ -1,38 +1,32 @@
 import React, { useState } from 'react';
 import { Hero } from '../components';
 import { useTransition } from 'react-spring';
-import { useEffect } from 'react';
 import backgrounds from '../fixtures/backgrounds.json';
+import useInterval from 'react-useinterval';
 
 export function HeroContainer() {
   const [index, setIndex] = useState(0);
   const increment = () => setIndex(index => (index + 1) % backgrounds.length)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      increment()
-    }, 3000);
-    return () => clearInterval(interval);
-  }, [index])
-
+  useInterval(increment, (document.hasFocus()) ? 3000 : null);
+  
   const transitions = useTransition(backgrounds[index], item => item.id, {
     from: {opacity: 0, transform: 'translateX(300px) scale(1.1)'},
     enter: {opacity: 1, transform: 'translateX(0px) scale(1)'},
     leave: {opacity: 0, transform: 'translateX(-300px) scale(0.9)'},
   });
-  
+
   return (
     <Hero>
       {
-        transitions.map(({item, props, key}) => {
-          return <Hero.Background
+        transitions.map(({item, props, key}) => (
+          <Hero.Background
             key={key}
             style={{ 
               backgroundImage: `url(${item.url})`,
               ...props 
             }}
           />
-        })
+        ))
       }
     </Hero>    
   )
