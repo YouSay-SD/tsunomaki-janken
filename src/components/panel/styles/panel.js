6a9d445df8e7ca60b04d54822762a4f9c5a6b8fe
YouSay-SD <yousay.sd@gmail.com>
2020-10-30 23:49:47 -0300

    clear code

--- src/components/panel/styles/panel.js
+++ src/components/panel/styles/panel.js
@@ -1,6 +1,5 @@
 import { animated } from 'react-spring';
 import styled from 'styled-components/macro';
-// import { respondBelow } from '../../../helpers/breakPoints';
 
 export const Container = styled.div`
   position: relative;
