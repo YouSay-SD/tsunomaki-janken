d5d469e351875365e8d8e4494c0fd6016bf7c029
YouSay-SD <yousay.sd@gmail.com>
2020-12-26 01:42:06 -0300

    Create Animation Modal

--- src/containers/modal.js
+++ src/containers/modal.js
@@ -6,39 +6,29 @@ export const ModalContainer = () => {
 
   const [ openModal, setOpenModal ] = useState(false);
 
-  const handleOpenModal = () => {
-    setOpenModal(!openModal);
-  }
-
   const showHideModal = useSpring({
-    // display: openModal ? 'flex' : 'none',
-    // visibility: openModal ? 'visible' : 'hidden',
-    // opacity: openModal ? 1 : 0,
     to: async (next, cancel) => {
-      openModal ? (
-        await next ({display: 'flex'}),
+      if (openModal) {
+        await next ({display: 'flex'})
         await next ({opacity: 1})
-      ) : (
-        await next ({opacity: 0}),
+      } else {
+        await next ({opacity: 0})
         await next ({display: 'none'})
-      )
+      }
     },
-    from: {opacity: 0, display: 'none'}
+    from: {opacity: 0, display: 'none'},
+    config: {duration: 300}
   });
-
-  // const scaleModal = useSpring({
-  //   transform: openModal ? 'scale(1.1)' : 'initial'
-  // })
 
   return (
     <Modal>
-      <button onClick={handleOpenModal}>Share Result & leave a message</button>
+      <button onClick={() => setOpenModal(!openModal)}>Share Result & leave a message</button>
       <Modal.Form style={showHideModal}>
         <Modal.Title>Leave your message here, it will be posted on the message board</Modal.Title>
         <Modal.Message placeholder="Your Message..." />
         <button type="submit">Send</button>
       </Modal.Form>
-      <Modal.Background style={showHideModal} onClick={handleOpenModal} />
+      <Modal.Background style={showHideModal} onClick={() => setOpenModal(!openModal)} />
     </Modal>
   )
 }
